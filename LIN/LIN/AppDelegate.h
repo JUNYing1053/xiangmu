a26b28342c8da5b7d5e53924e0e3ec2d97e8e92c
JUNYing1053 <Y1169306008@163.com>
2016-01-13 14:15:45 +0800

    新的view

---
+++ LIN/LIN/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LIN
+//
+//  Created by June on 16/1/13.
+//  Copyright © 2016年 Jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
